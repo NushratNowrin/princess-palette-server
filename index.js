bda27484c3cad0383ed58e82e3f57aebbf8789cd
NushratNowrin <nowrin2023@gmail.com>
2023-05-23 22:49:19 +0600

    vercel error fixing

--- index.js
+++ index.js
@@ -24,7 +24,6 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
 
     const database = client.db("toysDB");
     const toyCollection = database.collection("toys");
