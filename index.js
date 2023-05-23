a0f75648850fc3219f312810197fd369c39dd6d2
NushratNowrin <nowrin2023@gmail.com>
2023-05-23 06:14:39 +0600

    successfully connected to mongodb

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-require ('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -9,10 +9,13 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// from database deployment
+const user= process.env.DB_USER;
+const pass= process.env.DB_PASS;
+
+console.log(user, pass)
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.r1co4vf.mongodb.net/?retryWrites=true&w=majority`;
-console.log('The Uri', uri)
+// console.log(uri)
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -27,26 +30,23 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-
-    const database = client.db("toysDB");
-    const userCollection = database.collection("toys");
-
-
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    // await client.close();
+    await client.close();
   }
 }
 run().catch(console.dir);
+
 
 // start getting data
 
 app.get("/", (req, res) => {
 	res.send("Princess Palette Server is running!");
 });
+
 
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`);
