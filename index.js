8aa117ea6c4595af727b106b01b325df906d85be
NushratNowrin <nowrin2023@gmail.com>
2023-05-23 22:46:43 +0600

    vercel setup done

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const database = client.db("toysDB");
     const toyCollection = database.collection("toys");
